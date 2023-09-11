35342305ec8793a671193c4b913009808a49cc9d
robby <robby@DESKTOP-EFN5E7N>
2023-09-11 00:27:24 -0500

    Add project files.

---
+++ SaleSystem.IOC/Dependencia.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SaleSystem.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+//using SaleSystem.DAL.Interfaces;
+//using SaleSystem.DAL.Implementacion;
+//using SaleSystem.BLL.Interfaces;
+//using SaleSystem.BLL.Implementacion;
+
+namespace SaleSystem.IOC
+{
+    public static class Dependencia
+    {
+        public static void DependencyInjection(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DbventaContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+        }
+    }
+}
